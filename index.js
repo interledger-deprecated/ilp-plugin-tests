4adce0d9167f45ea700a156c54a220976c2994b3
Ben Sharafian <benms64@gmail.com>
2016-07-20 16:00:43 +0200

    fix: parse options from json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const plugin = require(process.env.ILP_PLUGIN_TEST)
-const opts = process.env.ILP_PLUGIN_OPTS
+const opts = JSON.parse(process.env.ILP_PLUGIN_OPTS)
 
 if (!plugin || !opts) {
   throw new Error('FATAL: ILP_PLUGIN_TEST and ILP_PLUGIN_OPTS must both be environment variables')
